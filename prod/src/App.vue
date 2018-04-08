681a9bb607fd945027c6d6e773cb52b5a5f95d0c
Mykola <chmutkola@gmail.com>
2018-04-08 23:11:54 +0300

    side bar

--- prod/src/App.vue
+++ prod/src/App.vue
@@ -1,13 +1,25 @@
 <template>
+<div>
+ <div class="header" >
+    <i class='ion-navicon' @click="show = !show" ></i>
+    <aside v-show="show" transition="slide" class="headre__menu" >
+    </aside>
+  </div>
   <div id="app">
     <img src="./assets/logo.png">
     <router-view/>
   </div>
+</div>
 </template>
 
 <script>
 export default {
   name: 'App',
+  data() {
+    return {
+      show:  false,
+    }
+  }
 };
 </script>
 
@@ -18,6 +30,35 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
+  .header{
+  height: 60px;
+  background-color: rgba(50, 50, 50, 1);
+  box-shadow:0 0 10px rgba(0,0,0,0.5);
+  display: flex;
+  align-items: center;
+  width: 100%;
+  z-index: 10;
+  }
+  .ion-navicon{
+    font-size: 30px;
+    color: white;
+    padding: 10px;
+    cursor: pointer;
+  }
+.headre__menu{
+  position: absolute;
+  left:0;
+  top: 0;
+  width: 200px;
+  min-height: 100%;
+  background-color:  rgba(50, 50, 50, 0.8);
+  z-index: -10;
+  }
+  .slide-transition{
+    transition: all .3s cubic-bezier(.65, 0.5, 0.56, 1);
+  }
+  .slide-enter,  .slide-leave{
+left: -100%;
+  }
 </style>
