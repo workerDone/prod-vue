9ce2850ab77ca970ea76da18532a39be217c1c37
name <chmutkola@gmail.com>
2018-04-19 09:01:40 +0300

    http

--- prod/src/components/Settings.vue
+++ prod/src/components/Settings.vue
@@ -1,15 +1,9 @@
 <template>
-  <div class="hello">
-    <button @click="moveLine" >hello</button>
-    <input v-model="message"  >
-    <div v-for="(item, index) in array" >{{ item }}</div>
-    <button @click="show = !show" >show</button>
-    <div v-if="show" >man</div>
-    <div v-else >woman</div>
-    <div v-show="show" >man</div>
-    
+  <div>
+    <div v-for="item of message" class="content">
+      <div>{{ item.name }}</div>
+    </div>
   </div>
-
 </template>
 
 <script>
@@ -18,47 +12,25 @@ export default {
   data() {
     return {
       message: '',
-      picked : '',
-      msg: 'Welcome to Your Vue.js App',
-      array: [],
-      show: false,
     };
   },
+  created() {
+   this.$http.get('https://jsonplaceholder.typicode.com/users').then(
+     respons => respons.json(),
+     respons => console.log('join')
+   ).then(respons => 
+      {
+        this.message = respons;
+        console.log( this.message,2)
+      })
+  },
+
   methods: {
-
-    move: function (message) {
-      console.log(message);
-    },
-
-    moveLine: function () {
-      this.array.push(this.message);
-
-      this.move(this.message);
-    },
-
-    moveShow: function () {
-
-    },
-
   }
 
 };
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
+
 <style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
 </style>
